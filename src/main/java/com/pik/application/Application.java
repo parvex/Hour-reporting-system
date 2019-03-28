0aca2b539c96911035b0e7e358d28fd11def3ddb
Mateusz <matk100.100@gmail.com>
2019-03-28 18:30:33 +0100

    Maven commented

--- src/main/java/com/pik/application/Application.java
+++ src/main/java/com/pik/application/Application.java
@@ -2,12 +2,15 @@ package com.pik.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
 
+    }
 }
