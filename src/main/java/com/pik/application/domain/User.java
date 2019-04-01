fc07e5dc892cec9f72b2f0bca000313b4c42f97e
grebacz <gabrielrebacz@gmail.com>
2019-04-02 00:34:46 +0200

    -db config -init entity schema -user api -schema of home controller

---
+++ src/main/java/com/pik/application/domain/User.java
@@ -0,0 +1,86 @@
+package com.pik.application.domain;
+
+import org.hibernate.annotations.NaturalId;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String surname;
+
+    @NotBlank
+    @NaturalId
+    private String email;
+
+    @NotBlank
+    private String passwordHash;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date registeredAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getRegisteredAt() {
+        return registeredAt;
+    }
+
+    public void setRegisteredAt(Date registeredAt) {
+        this.registeredAt = registeredAt;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+}
---
+++ src/main/java/com/pik/application/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.pik.application.repository;
+
+import com.pik.application.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
