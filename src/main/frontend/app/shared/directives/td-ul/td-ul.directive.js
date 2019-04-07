32581b402899a626937c0123b8cd2f2f5318a433
wkurek <w.kurek2@gmail.com>
2019-04-07 18:02:18 +0200

    addition of ng-table pagination

--- src/main/frontend/app/shared/directives/td-ul/td-ul.directive.js
+++
@@ -1,9 +0,0 @@
-angular.module("reportingApp").directive("tdUl", function() {
-  return {
-    restrict: "E",
-    scope: {
-      list: "="
-    },
-    templateUrl: "shared/directives/td-ul/td-ul.template.html"
-  };
-});
