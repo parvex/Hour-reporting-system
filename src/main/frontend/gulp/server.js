c766f9634dc2ca483be9919791774949432c0634
wkurek <w.kurek2@gmail.com>
2019-05-05 13:13:58 +0200

    proxy to localhost:8080

--- src/main/frontend/gulp/server.js
+++ src/main/frontend/gulp/server.js
@@ -1,23 +1,26 @@
-'use strict';
+"use strict";
 
-var path = require('path');
-var gulp = require('gulp');
-var conf = require('./conf');
+var path = require("path");
+var gulp = require("gulp");
+var conf = require("./conf");
 
-var browserSync = require('browser-sync');
-var browserSyncSpa = require('browser-sync-spa');
+var browserSync = require("browser-sync");
+var browserSyncSpa = require("browser-sync-spa");
 
-var util = require('util');
+var util = require("util");
 
-var proxyMiddleware = require('http-proxy-middleware');
+var proxyMiddleware = require("http-proxy-middleware");
 
 function browserSyncInit(baseDir, browser) {
-  browser = browser === undefined ? 'default' : browser;
+  browser = browser === undefined ? "default" : browser;
 
   var routes = null;
-  if(baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(conf.paths.src) !== -1)) {
+  if (
+    baseDir === conf.paths.src ||
+    (util.isArray(baseDir) && baseDir.indexOf(conf.paths.src) !== -1)
+  ) {
     routes = {
-      '/bower_components': 'bower_components'
+      "/bower_components": "bower_components"
     };
   }
 
@@ -33,31 +36,36 @@ function browserSyncInit(baseDir, browser) {
    *
    * For more details and option, https://github.com/chimurai/http-proxy-middleware/blob/v0.9.0/README.md
    */
-  // server.middleware = proxyMiddleware('/users', {target: 'http://jsonplaceholder.typicode.com', changeOrigin: true});
+  server.middleware = proxyMiddleware("/api", {
+    target: "http://localhost:8080",
+    changeOrigin: true
+  });
 
   browserSync.instance = browserSync.init({
-    startPath: '/',
+    startPath: "/",
     server: server,
     browser: browser
   });
 }
 
-browserSync.use(browserSyncSpa({
-  selector: '[ng-app]'// Only needed for angular apps
-}));
+browserSync.use(
+  browserSyncSpa({
+    selector: "[ng-app]" // Only needed for angular apps
+  })
+);
 
-gulp.task('serve', ['watch'], function () {
-  browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src]);
+gulp.task("serve", ["watch"], function() {
+  browserSyncInit([path.join(conf.paths.tmp, "/serve"), conf.paths.src]);
 });
 
-gulp.task('serve:dist', ['build'], function () {
+gulp.task("serve:dist", ["build"], function() {
   browserSyncInit(conf.paths.dist);
 });
 
-gulp.task('serve:e2e', ['inject'], function () {
-  browserSyncInit([conf.paths.tmp + '/serve', conf.paths.src], []);
+gulp.task("serve:e2e", ["inject"], function() {
+  browserSyncInit([conf.paths.tmp + "/serve", conf.paths.src], []);
 });
 
-gulp.task('serve:e2e-dist', ['build'], function () {
+gulp.task("serve:e2e-dist", ["build"], function() {
   browserSyncInit(conf.paths.dist, []);
 });
