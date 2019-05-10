787ac9c4393ff27cbcc7821b51a6b17e4e7e567e
wkurek <w.kurek2@gmail.com>
2019-05-10 12:59:44 +0200

    basic template elements

--- src/main/frontend/src/app/log-calendar/log-calendar.component.js
+++ src/main/frontend/src/app/log-calendar/log-calendar.component.js
@@ -7,4 +7,34 @@ angular
   })
   .controller("LogCalendarCtrl", function($scope) {
     const lcCtrl = this;
+    lcCtrl.fliterCriteria = new Object();
+
+    lcCtrl.openDateFromPickerModal = openDateFromPickerModal;
+    lcCtrl.openDateToPickerModal = openDateToPickerModal;
+
+    lcCtrl.dateOptions = {
+      dateDisabled: disabled,
+      minDate: getMinDate(),
+      startingDay: 1
+    };
+
+    function openDateFromPickerModal() {
+      lcCtrl.dateFromPickerOpened = true;
+    }
+
+    function openDateToPickerModal() {
+      lcCtrl.dateToPickerOpened = true;
+    }
+
+    // Disable weekend selection
+    function disabled(data) {
+      var date = data.date,
+        mode = data.mode;
+      return mode === "day" && (date.getDay() === 0 || date.getDay() === 6);
+    }
+
+    function getMinDate() {
+      //TODO: return min date
+      return new Date();
+    }
   });
