a7c5ebb8170233e4f5327363404a59068ae03950
wkurek <w.kurek2@gmail.com>
2019-05-11 13:55:16 +0200

    report-details form fields

--- src/main/frontend/src/app/report-details/report-details.component.js
+++ src/main/frontend/src/app/report-details/report-details.component.js
@@ -5,12 +5,61 @@ angular
     controller: "ReporteDetailsCtrl",
     controllerAs: "rdCtrl"
   })
-  .controller("ReporteDetailsCtrl", function(reportId, $uibModalInstance) {
+  .controller("ReporteDetailsCtrl", function(
+    reportId,
+    $uibModalInstance,
+    ReportsService,
+    ProjectsService,
+    EmployeesService
+  ) {
     const rdCtrl = this;
     rdCtrl.reportId = reportId;
 
+    rdCtrl.openDatePickerModal = openDatePickerModal;
+
+    rdCtrl.provideProjects = provideProjects;
+    rdCtrl.provideEmployees = provideEmployees;
+
     rdCtrl.save = save;
     rdCtrl.cancel = cancel;
+
+    rdCtrl.dateOptions = {
+      dateDisabled: disabled,
+      minDate: getMinDate(),
+      startingDay: 1
+    };
+
+    if (reportId) {
+      ReportsService.getReport(reportId).then(function(response) {
+        rdCtrl.report = response;
+      });
+    } else {
+      rdCtrl.report = {};
+    }
+
+    function getMinDate() {
+      //TODO: return min date
+      return new Date();
+    }
+
+    // Disable weekend selection
+    function disabled(data) {
+      var date = data.date,
+        mode = data.mode;
+      return mode === "day" && (date.getDay() === 0 || date.getDay() === 6);
+    }
+
+    function openDatePickerModal() {
+      rdCtrl.datePickerOpened = true;
+    }
+
+    function provideProjects(request) {
+      return ProjectsService.getProjects(request);
+    }
+
+    function provideEmployees(request) {
+      return EmployeesService.getEmployees(request);
+    }
 
     function save() {
       //TODO: implement this function
