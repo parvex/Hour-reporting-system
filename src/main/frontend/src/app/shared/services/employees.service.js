da26ec544259260edad891c31ca11d89c6a6e4a6
wkurek <w.kurek2@gmail.com>
2019-05-11 09:19:55 +0200

    employees multi select

--- src/main/frontend/src/app/shared/services/employees.service.js
+++ src/main/frontend/src/app/shared/services/employees.service.js
@@ -6,16 +6,12 @@ angular
     service.getManagers = getManagers;
 
     function getEmployees(request) {
-      return Promise.resolve({ list: [] });
-
       return $http.post("/api/employees", request).then(function(response) {
         return response.data;
       });
     }
 
     function getManagers(request) {
-      return Promise.resolve({ list: [] });
-
       return $http.post("/api/managers", request).then(function(response) {
         return response.data;
       });
