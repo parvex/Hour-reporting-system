3117c2adbcc7d797e419c20edb8f3b8ae9ea54e5
Mateusz Krawczyk <matk100.100@gmail.com>
2019-05-16 20:00:35 +0200

    Work reports added Admin

--- src/main/java/com/pik/application/repository/WorkReportRepository.java
+++ src/main/java/com/pik/application/repository/WorkReportRepository.java
@@ -32,7 +32,7 @@ public interface WorkReportRepository extends JpaRepository<WorkReport, Long> {
     @Query("SELECT new com.pik.application.dto.WRepDate(w.date, w.hours, w.user.id, w.user.name, w.user.surname, w.comment, w.accepted," +
             " w.project.id, w.project.name) FROM WorkReport w WHERE w.date BETWEEN :dateFrom AND :dateTo AND (w.user.id IN (:employeeIds) " +
             " OR COALESCE(:employeeIds, NULL) IS NULL) AND (w.project.id IN (:projectIds) OR COALESCE(:projectIds, NULL) IS NULL)" +
-            " AND w.user.supervisor.id = :loggedId OR w.user.id = :loggedId ORDER BY w.date DESC")
+            " AND w.user.supervisor.id = :loggedId OR w.user.id = :loggedId OR :loggedId = 1811 ORDER BY w.date DESC")
     List<WRepDate> findByDateBetweenOrderByDateAsc(Date dateFrom, Date dateTo, @Nullable List<Long> employeeIds, @Nullable List<Long> projectIds, Long loggedId);
 
     @Query("SELECT new com.pik.application.dto.WorkReportExtraInfo(w.id, w.date, w.hours, w.user.id, w.user.name, w.user.surname, w.project.id, w.project.name, w.comment, w.accepted)" +
