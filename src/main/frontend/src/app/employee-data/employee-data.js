ac5fd28b5d554c72c8753b2a6efc940788e28903
hborkows <hubert.borkowski@hotmail.com>
2019-05-28 19:59:21 +0200

    Deleted empty collection message

--- src/main/frontend/src/app/employee-data/employee-data.js
+++ src/main/frontend/src/app/employee-data/employee-data.js
@@ -9,7 +9,6 @@ angular
   ) {
     $scope.projects = [];
     $scope.usedLeave;
-    $scope.noReports = true;
     var userId = $rootScope.userSelectId;
 
     $scope.getProjects = function() {
@@ -22,7 +21,6 @@ angular
         EmployeesService.getProjectsHours(userId).then(function (response) {
           if(response) {
             $scope.projects = response;
-            $scope.noReports = false;
             $rootScope.userSelectId = null;
           }
         }, function(error) {
