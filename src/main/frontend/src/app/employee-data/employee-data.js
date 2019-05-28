44e8f09fe9db0770171a1c470c68caa9a9314e01
hborkows <hubert.borkowski@hotmail.com>
2019-05-28 18:07:24 +0200

    Fixed setting global parameter to null

--- src/main/frontend/src/app/employee-data/employee-data.js
+++ src/main/frontend/src/app/employee-data/employee-data.js
@@ -9,23 +9,22 @@ angular
   ) {
     $scope.projects = [];
     $scope.usedLeave;
-    var userId = null;
+    $scope.noReports = true;
+    var userId = $rootScope.userSelectId;
 
     $scope.getProjects = function() {
-      console.log($rootScope.userSelectId);
-      console.log("check");
-
-      if($rootScope.userSelectId) {
-        userId = $rootScope.userSelectId;
-      }
-      else {
+      if(userId === null) {
         userId = $cookies.getObject("user").id;
       }
 
       if(userId != null)
       {
         EmployeesService.getProjectsHours(userId).then(function (response) {
+          if(response) {
             $scope.projects = response;
+            $scope.noReports = false;
+            $rootScope.userSelectId = null;
+          }
         }, function(error) {
             console.log("Error receiving projects");
         });
@@ -33,11 +32,7 @@ angular
     }
 
     $scope.getUsedLeave = function() {
-      if($rootScope.userSelectId) {
-        userId = $rootScope.userSelectId.toString();
-        $rootScope.userSelectId = null;
-      }
-      else {
+      if(userId === null) {
         userId = $cookies.getObject("user").id;
       }
 
@@ -45,6 +40,7 @@ angular
       {
         EmployeesService.getUsedLeave(userId).then(function (response) {
           $scope.usedLeave = response;
+          $rootScope.userSelectId = null;
         }, function (error) {
           console.log("Error getting used leave");
         })
